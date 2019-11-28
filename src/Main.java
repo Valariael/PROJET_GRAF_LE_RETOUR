b7e1cc63dda5e01dc285e1d17d3d2518b474990f
Axel Ledermann <33054741+Valariael@users.noreply.github.com>
2019-11-28 19:06:25 +0100

    removed old function

--- src/Main.java
+++ src/Main.java
@@ -20,36 +20,4 @@ public class Main {
             e.printStackTrace();
         }
     }
-
-    private static PertGraf getPertFromFile(String pathToFile) throws IOException, InvalidFormatException {
-        File file = new File(pathToFile);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        PertGraf p = new PertGraf();
-        HashMap<Task, Integer> durations = new HashMap<>();
-
-        String s;
-        while ((s = br.readLine()) != null) {
-            if (s.trim().startsWith("#")) {
-                System.out.println("> commented line read");
-            } else {
-                String[] parts = s.split(",");
-                if(parts.length < 4) throw new InvalidFormatException();
-
-                Task task = new Task(parts[0].trim(), parts[1].trim());
-                p.addNode(task);
-                durations.put(task, Integer.parseInt(parts[2].trim()));
-
-                if(!parts[3].trim().equals("-")) {
-                    for(int i = 3; i < parts.length; i++) {
-                        Task t = new Task(parts[i].trim());
-                        Task toTask = new Task(task.getName(), task.getLabel(), durations.get(t));
-                        toTask.setToWeightActivated(true);
-                        p.addEdge(t, toTask);
-                    }
-                }
-            }
-        }
-
-        return p;
-    }
 }
